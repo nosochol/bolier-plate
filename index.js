286926aa5c64efc08a964bc48e0aace5557b3778
rhoseungchol <nosochol@naver.com>
2020-10-07 21:47:34 +0900

    password

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://nosochol:1004@cluster0.48re6.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false 
 }).then(()=> console.log('mongodbd connnected...'))
 .catch(err => console.log('error...'))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
+  res.send('Hello World! 안녕하세요 22222')
+})
+
+app.post('/register',(req, res) => {
+  //login clint catch into database save
+
+   const user = new User(req.body) 
+   user.save((err,doc) =>{
+     if(err) return res.json({ success: false, err})
+     return res.status(200).json({
+       success:true
+     })
+   });
+
 })
 
 app.listen(port, () => {
